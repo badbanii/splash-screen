10ec7c82f94c3d904af6fdd5959e26bd2ce68b30
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-19 12:53:54 +0200

    Updated UI

--- SplashScreen/ContentView.swift
+++ SplashScreen/ContentView.swift
@@ -2,8 +2,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSplash = true
+    
     var body: some View {
-        Text("SplashScreen")
+          ZStack {
+              if showSplash {
+                  SplashScreen()
+                      .onAppear(perform: {
+                          DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                              withAnimation {
+                                  showSplash = false
+                              }
+                          }
+                      })
+              } else {
+                  MainScreen()
+              }
+          }
+      }
+}
+
+struct SplashScreen: View {
+    var body: some View {
+        Text("Splash Screen").foregroundColor(Color.pink)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MainScreen: View {
+    var body: some View {
+        Text("Main Screen").foregroundColor(Color.pink)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
