127a2f48fedd769b3bec982e26d54227b39b637c
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-19 12:56:28 +0200

    Updated UI

--- SplashScreen/ContentView.swift
+++ SplashScreen/ContentView.swift
@@ -5,37 +5,41 @@ struct ContentView: View {
     @State private var showSplash = true
     
     var body: some View {
-          ZStack {
-              if showSplash {
-                  SplashScreen()
-                      .onAppear(perform: {
-                          DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                              withAnimation {
-                                  showSplash = false
-                              }
-                          }
-                      })
-              } else {
-                  MainScreen()
-              }
-          }
-      }
+        ZStack {
+            if showSplash {
+                SplashScreen()
+                    .onAppear(perform: {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            withAnimation {
+                                showSplash = false
+                            }
+                        }
+                    })
+            } else {
+                MainScreen()
+            }
+        }
+    }
 }
 
 struct SplashScreen: View {
     var body: some View {
-        Text("Splash Screen").foregroundColor(Color.pink)
+        Text("Splash Screen")
+            .foregroundColor(Color.indigo)
+            .font(.system(size: 36))
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
+            .background(Color.yellow)
             .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct MainScreen: View {
     var body: some View {
-        Text("Main Screen").foregroundColor(Color.pink)
+        Text("Main Screen")
+            .foregroundColor(Color.yellow)
+            .font(.system(size: 36))
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
+            .background(Color.indigo)
             .edgesIgnoringSafeArea(.all)
     }
 }
